904593c0ecb17b551329f97b10bfbf96c4f427d7
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-08-21 14:47:14 +0800

    resolve with cdn dependencies
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import * as THREE from "/node_modules/three";
-import $ from "/node_modules/jquery";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.132.2/build/three.module.js";
+import $ from "jquery";
 var container = document.getElementById("globeCanvas");
 
 //SETUP SCENE
