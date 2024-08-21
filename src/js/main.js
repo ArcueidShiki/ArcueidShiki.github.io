be8b123750df88fc01ece5bf8d14125600a7b4f0
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-08-21 14:12:06 +0800

    fix relative references
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import $ from "jquery";
+import * as THREE from "/node_modules/three";
+import $ from "/node_modules/jquery";
 var container = document.getElementById("globeCanvas");
 
 //SETUP SCENE
